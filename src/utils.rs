7955d7c8cc8ee8d5261bcbe4cdefe6ad080d5b93
Rose Peck <olaf223224@gmail.com>
2021-06-27 20:53:22 -0500

    Hex tile selection
    It almost works. There's some weird accuracy drift in the Y direction. To be fixed later:tm:

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{math::{Mat2}, prelude::*};
 
 #[derive(Debug)]
 pub struct NotInWindowError;
@@ -7,7 +7,7 @@ pub struct NotInWindowError;
 pub fn fetch_mouse_coordinates(
     windows: &Windows,
     camera_transform: &Transform,
-) -> Result<Vec4, NotInWindowError> {
+) -> Result<Vec2, NotInWindowError> {
     // Get the primary window
     let window = windows.get_primary().unwrap();
 
@@ -21,14 +21,49 @@ pub fn fetch_mouse_coordinates(
         let p = pos - size / 2.0;
 
         // Apply the camera transform
-        return Ok(camera_transform.compute_matrix() * p.extend(0.0).extend(1.0))
+        let transform_vec4 = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
+        // We only care about the first two fields
+        let coordinates = Vec2::new(transform_vec4.x, transform_vec4.y); 
+
+        return Ok(coordinates)
     } else {
 		return Err(NotInWindowError)
 	}
 }
 
+// FIXME: this is very inefficient
 /// Returns the tile coordinates at the given world coordinates
-pub fn tile_from_world_coordinates(world_position: Vec4) -> UVec2 {
-	// FIXME: add logic
-	UVec2::new(1,1)
+pub fn tile_from_world_coordinates(
+    world_position: Vec2, 
+    tile_size: Vec2
+) -> Option<UVec2> {
+    
+    let hex_basis = Mat2::from_cols(Vec2::new(3f32.sqrt()/3.0, 0.0), Vec2::new(-1.0/3.0, 2.0/3.0));
+    let tile_offset = tile_size * -0.5;
+    
+    let hex_basis_position = (hex_basis * (world_position + tile_offset)) / (tile_size.y * 0.5);
+
+    let tile_position = hex_round(hex_basis_position);
+    if tile_position.min_element() < 0.0 {
+        return None;
+    } else {
+        return Some(tile_position.as_u32());
+    }
+}
+
+fn hex_round(coords: Vec2) -> Vec2 {
+    let cube_coords = coords.extend( -1.0 * coords.x - coords.y);
+    
+    let mut rounded_cube_coords = cube_coords.round();
+    let coords_diff = rounded_cube_coords - cube_coords;
+    
+    if coords_diff.x > coords_diff.y && coords_diff.x > coords_diff.z {
+        rounded_cube_coords.x = -1.0 * rounded_cube_coords.y - rounded_cube_coords.z;
+    } else if coords_diff.y > coords_diff.z {
+        rounded_cube_coords.y = -1.0 * rounded_cube_coords.x - rounded_cube_coords.z;
+    } else {
+        rounded_cube_coords.z = -1.0 * rounded_cube_coords.x - rounded_cube_coords.y;
+    }
+
+    return rounded_cube_coords.truncate();
 }
